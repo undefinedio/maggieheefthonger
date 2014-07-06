0fa4720d218fe79a82973240daf60015bfd1e800
koray.sels <koray.sels@gmail.com>
2014-07-06 20:57:31 +0200

    dunno better gitignore ?

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // sup sup
 var sound = new Howl({
     urls: ['../sounds/maggie_breath.mp3'],
-    autoplay: false,
+    autoplay: true,
     loop: true,
     volume: 0.5,
     onend: function () {
@@ -11,25 +11,12 @@ var sound = new Howl({
 
 $(function ($) {
 
-//    $('body').mousemove(function (kmouse) {
-//        var r = 5;
-//        var leftPosition = $("#lefteye").position();
-//        var rightPosition = $("#righteye").position();
-//        var xlEl = leftPosition.left;
-//        var ylEl = leftPosition.top;
-//
-//        var ym = kmouse.pageY - ylEl + 20;
-//        var xm = kmouse.pageX - xlEl + 20;
-//
-//        $('.mousfollower').css({'top': ym, 'left': xm});
-//
-//        console.log("r => " + r + ", xm => " + xm + "ym => " + ym);
-//
-//    });
+    var mouseXLeft = 0, mouseYLeft = 0, mouseXRight = 0, mouseYRight = 0, limitX = 70, limitY = 14;
+//    initializing but not used
+    var ang, i = 0;
 
-
-    var mouseXLeft = 0, mouseYLeft = 0, mouseXRight = 0, mouseYRight = 0, limitX = 70, limitY = 11;
     $(window).mousemove(function (e) {
+
         var offsetLeft = $('#left').offset();
         mouseXLeft = Math.min(e.pageX - offsetLeft.left, limitX);
         mouseYLeft = Math.min(e.pageY - offsetLeft.top, limitY);
@@ -45,16 +32,25 @@ $(function ($) {
 
     var leftPupil = $('#leftpupil'), xp = 0, yp = 0,
         leftLoop = setInterval(function () {
-            xp += (mouseXLeft - xp) / 1  ;
-            yp += (mouseYLeft - yp) / 1  ;
+            xp += (mouseXLeft - xp) / 1;
+            yp += (mouseYLeft - yp) / 1;
+//            leftPupil.css({left: (randomFloatBetween(-5, 5,1) + xp), top: (randomFloatBetween(-5, 5,1) + yp)});
             leftPupil.css({left: xp, top: yp});
         }, 30);
     var rightPupil = $('#rightpupil'), xp = 0, yp = 0,
         rightLoop = setInterval(function () {
             xp += (mouseXRight - xp) / 1;
             yp += (mouseYRight - yp) / 1;
+//            rightPupil.css({left: (randomFloatBetween(-5, 5,1) + xp), top: (randomFloatBetween(-5, 5,1) + yp)});
             rightPupil.css({left: xp, top: yp});
         }, 30);
 
+    // Random float between
+    function randomFloatBetween(minValue,maxValue,precision){
+        if(typeof(precision) == 'undefined'){
+            precision = 2;
+        }
+        return parseFloat(Math.min(minValue + (Math.random() * (maxValue - minValue)),maxValue).toFixed(precision));
+    }
 });
 
--- src/js/main.js
+++ src/js/main.js
@@ -10,8 +10,13 @@ var sound = new Howl({
 });
 
 $(function ($) {
-    var mouseXLeft = 0, mouseYLeft = 0, mouseXRight = 0, mouseYRight = 0, limitX = 70, limitY = 11;
+
+    var mouseXLeft = 0, mouseYLeft = 0, mouseXRight = 0, mouseYRight = 0, limitX = 70, limitY = 14;
+//    initializing but not used
+    var ang, i = 0;
+
     $(window).mousemove(function (e) {
+
         var offsetLeft = $('#left').offset();
         mouseXLeft = Math.min(e.pageX - offsetLeft.left, limitX);
         mouseYLeft = Math.min(e.pageY - offsetLeft.top, limitY);
@@ -27,16 +32,25 @@ $(function ($) {
 
     var leftPupil = $('#leftpupil'), xp = 0, yp = 0,
         leftLoop = setInterval(function () {
-            xp += (mouseXLeft - xp) / 1  ;
-            yp += (mouseYLeft - yp) / 1  ;
+            xp += (mouseXLeft - xp) / 1;
+            yp += (mouseYLeft - yp) / 1;
+//            leftPupil.css({left: (randomFloatBetween(-5, 5,1) + xp), top: (randomFloatBetween(-5, 5,1) + yp)});
             leftPupil.css({left: xp, top: yp});
         }, 30);
     var rightPupil = $('#rightpupil'), xp = 0, yp = 0,
         rightLoop = setInterval(function () {
             xp += (mouseXRight - xp) / 1;
             yp += (mouseYRight - yp) / 1;
+//            rightPupil.css({left: (randomFloatBetween(-5, 5,1) + xp), top: (randomFloatBetween(-5, 5,1) + yp)});
             rightPupil.css({left: xp, top: yp});
         }, 30);
 
+    // Random float between
+    function randomFloatBetween(minValue,maxValue,precision){
+        if(typeof(precision) == 'undefined'){
+            precision = 2;
+        }
+        return parseFloat(Math.min(minValue + (Math.random() * (maxValue - minValue)),maxValue).toFixed(precision));
+    }
 });
 
