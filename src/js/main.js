2ecca766c04d1fb83057bf8ecb2f686bdfc7f2f8
koray.sels <koray.sels@gmail.com>
2014-07-06 15:59:33 +0200

    autobreath added great :D

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
 // sup sup
+var sound = new Howl({
+    urls: ['../sounds/maggie_breath.mp3'],
+    autoplay: true,
+    loop: true,
+    volume: 0.5,
+    onend: function() {
+        console.log('Finished!');
+    }
+});
